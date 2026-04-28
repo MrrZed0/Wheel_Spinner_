9a629850bd82e5830d02aa6cb39235e63302e71a
MrrZed0 <154046655+MrrZed0@users.noreply.github.com>
2026-04-28 01:02:07 -0400

    Add files via upload

---
+++ Wheel_Spinner/Wheel Winner/script.js
@@ -0,0 +1,28 @@
+const params = new URLSearchParams(window.location.search);
+document.getElementById("text").textContent = params.get("name")||"Username";
+
+// do this for 5 seconds
+var duration = 5 * 1000;
+var end = Date.now() + duration;
+
+(function frame() {
+	// launch a few confetti from the left edge
+	confetti({
+		particleCount: 7,
+		angle: 60,
+		spread: 55,
+		origin: { x: 0 }
+	});
+	// and launch a few from the right edge
+	confetti({
+		particleCount: 7,
+		angle: 120,
+		spread: 55,
+		origin: { x: 1 }
+	});
+
+	// keep going until we are out of time
+	if (Date.now() < end) {
+		requestAnimationFrame(frame);
+	}
+})();
